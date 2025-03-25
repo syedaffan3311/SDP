9bd7c5db07882cb579bfbe4b41df263f074f3b92
affan <newoffiacc@gmail.com>
2025-03-25 10:05:03 +0530

    second commit

--- raduis.py
+++ raduis.py
@@ -1,3 +1,6 @@
 radius=int(input("Enter the radius of the circle: "))
 area=3.14*radius*radius
 print("Area of the circle is: ",area)
+radius=int(input("Enter the radius of the circle: "))
+area=3.14*radius*radius
+print("Area of the circle is: ",area)
